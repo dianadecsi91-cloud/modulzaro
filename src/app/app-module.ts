6d3ab8a9e764687a87b40ed3890ef3dd67bf2413
diana.decsi <dianadecsi91@gmail.com>
2026-06-20 15:32:52 +0200

    seed list finished

---
+++ src/app/app-module.ts
@@ -0,0 +1,18 @@
+import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing-module';
+import { App } from './app';
+import { List } from './list/list';
+import { Cart } from './cart/cart';
+import { Product } from './product/product';
+import { Create } from './create/create';
+import { Delete } from './delete/delete';
+
+@NgModule({
+  declarations: [App, List, Cart, Product, Create, Delete],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [provideBrowserGlobalErrorListeners()],
+  bootstrap: [App],
+})
+export class AppModule {}
---
+++ src/app/app-routing-module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { List } from './list/list';
+import { Cart } from './cart/cart';
+import { Create } from './create/create';
+import { Delete } from './delete/delete';
+import { Product } from './product/product';
+
+const routes: Routes = [
+  {path: "cart", component: Cart},
+  {path: "create", component: Create},
+  {path: "delete", component: Delete},
+  {path: "list", component: List},
+  {path: "product", component: Product}
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
