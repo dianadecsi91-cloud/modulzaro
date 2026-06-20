250911a460cf7fa09decc80891a69efd8c4679a0
diana.decsi <dianadecsi91@gmail.com>
2026-06-20 16:02:32 +0200

    guid installed and product.ts finished

--- src/app/product.ts
+++ src/app/product.ts
@@ -1,1 +1,11 @@
-export class Product {}
+import { Guid } from "guid-typescript"
+
+export class Product {
+    id: string = Guid.create().toString()
+    name: string = ""
+    category: string = ""
+    price: number | null = null
+    description: string = ""
+    quantity: number | null = null
+    availability: boolean = true
+}
